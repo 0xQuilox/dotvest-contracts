5dec1b4aaaff59d73913dd7d6992e803b9c4b241
0xQuiloxE <miftahudeentajudeen@gmail.com>
2025-08-24 11:45:57 +0100

    dieho

--- contracts/exchange/AMMPool.sol
+++ contracts/exchange/AMMPool.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./access/OwnableAndRoles.sol";
+import "../access/OwnableAndRoles.sol";
 
 /**
  * @title AMMPool
