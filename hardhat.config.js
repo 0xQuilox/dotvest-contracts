f20079f3a9198b8fa78fd3ccb003bb3f6956ff6c
0xQuiloxE <miftahudeentajudeen@gmail.com>
2025-08-24 11:11:40 +0100

    vdhjjcjs

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,22 +2,22 @@ import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.24", // Use a recent Solidity version
+const config = {
+  solidity: "0.8.24",
   networks: {
-    // Local network for fast testing
+    // Local Hardhat network
     hardhat: {
       chainId: 1337,
     },
-    // Polygon testnet
+    // Polygon Amoy testnet
     amoy: {
       url: `https://polygon-amoy.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
     // Polygon mainnet
     polygon: {
       url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
   etherscan: {
@@ -27,3 +27,5 @@ module.exports = {
     },
   },
 };
+
+export default config;
